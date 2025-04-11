cd0a5e69c5181db2b90bf7af0a920b638250cf33
Ania Nowacka <anna.nowacka146@gmail.com>
2025-04-11 11:20:15 +0200

    Initial commit

---
+++ frontend/viking-app/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="p-10 min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-gray-900">
+        🚀 Hola Amigos
+      </h1>
+      <p className="mt-4 text-gray-700">Its gonna be awesome!</p>
+    </div>
+  );
+}
+
+export default App;
