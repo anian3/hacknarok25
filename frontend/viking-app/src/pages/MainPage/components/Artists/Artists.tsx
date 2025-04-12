a78e92a3675afeb8da1e2adb51874237ab9f310b
Ania Nowacka <anna.nowacka146@gmail.com>
2025-04-12 22:31:28 +0200

    new architecture

---
+++ frontend/viking-app/src/pages/MainPage/components/Artists/Artists.tsx
@@ -0,0 +1,18 @@
+
+
+const Artists = () => {
+  return (
+    (
+        <div className="bg-white p-8 rounded-lg shadow text-center">
+          <h3 className="text-xl font-medium text-gray-700">
+            Featured Artists
+          </h3>
+          <p className="text-gray-500 mt-2">
+            No featured artists available at the moment.
+          </p>
+        </div>
+      )
+  )
+}
+
+export default Artists
---
+++ frontend/viking-app/src/pages/MainPage/components/Jobs/Jobs.tsx
@@ -0,0 +1,12 @@
+const Jobs = () => {
+  return (
+    <div className="bg-white p-8 rounded-lg shadow text-center">
+      <h3 className="text-xl font-medium text-gray-700">Job Listings</h3>
+      <p className="text-gray-500 mt-2">
+        No job listings available at the moment.
+      </p>
+    </div>
+  );
+};
+
+export default Jobs;
