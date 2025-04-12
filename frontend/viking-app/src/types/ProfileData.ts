b5e8ffc494a245f19bded3820f2ded662b40606e
Ania Nowacka <anna.nowacka146@gmail.com>
2025-04-12 20:21:15 +0200

    some example profile layout

---
+++ frontend/viking-app/src/types/ProfileData.ts
@@ -0,0 +1,4 @@
+import { ArtistProfileData } from "./ArtistProfileData";
+import { BusinessProfileData } from "./BusinessProfileData";
+
+export type ProfileData = ArtistProfileData | BusinessProfileData;
