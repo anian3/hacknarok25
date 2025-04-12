f0abcc863ed8d970928f85a90ad2b8b90db99b2c
Ania Nowacka <anna.nowacka146@gmail.com>
2025-04-12 23:36:12 +0200

    fixed icons

--- frontend/viking-app/src/components/Header/Header.tsx
+++ frontend/viking-app/src/components/Header/Header.tsx
@@ -24,31 +24,50 @@ function Header() {
         {/* Desktop Navigation */}
         <div className="hidden md:flex justify-end space-x-8 lg:space-x-16 flex-1">
           <div className="flex flex-col items-center justify-center">
-            <img src={bank_outline} alt="Bank" className="h-8 w-8" />
-            <Link to="/muse" className="hover:underline text-lg text-white ml-0">
-              Choose a muse
+            <Link
+              to="/muse"
+              className="flex flex-col items-center hover:underline text-lg text-white"
+            >
+              <img src={bank_outline} alt="Bank" className="h-8 w-8 mb-1" />
+              <span>Choose a muse</span>
             </Link>
           </div>
           <div className="flex flex-col items-center justify-center">
-            <img src={hand_extended_outline} alt="Hand" className="h-8 w-8" />
-            <Link to="/community" className="hover:underline text-lg text-white ml-0">
-              Community
+            <Link
+              to="/community"
+              className="flex flex-col items-center hover:underline text-lg text-white"
+            >
+              <img
+                src={hand_extended_outline}
+                alt="Hand"
+                className="h-8 w-8 mb-1"
+              />
+              <span>Community</span>
             </Link>
           </div>
           <div className="flex flex-col items-center justify-center">
-            <img src={chat_outline} alt="Messages" className="h-8 w-8" />
-            <Link to="/messages" className="hover:underline text-lg text-white ml-0">
-              Messages
+            <Link
+              to="/messages"
+              className="flex flex-col items-center hover:underline text-lg text-white"
+            >
+              <img src={chat_outline} alt="Messages" className="h-8 w-8 mb-1" />
+              <span>Messages</span>
             </Link>
           </div>
           <div className="flex flex-col items-center justify-center">
-            <img src={profile_outline} alt="Profile" className="h-8 w-8" />
-            <Link to="/profile" className="hover:underline text-lg text-white ml-0">
-              Profile
+            <Link
+              to="/profile"
+              className="flex flex-col items-center hover:underline text-lg text-white"
+            >
+              <img
+                src={profile_outline}
+                alt="Profile"
+                className="h-8 w-8 mb-1"
+              />
+              <span>Profile</span>
             </Link>
           </div>
         </div>
-
         {/* Mobile Menu Button */}
         <button
           className="md:hidden text-white focus:outline-none"
